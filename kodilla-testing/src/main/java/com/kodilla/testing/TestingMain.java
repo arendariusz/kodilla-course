f2a6c18f70cc84e040feac19957961001a60bcb3
arendariusz <d.arendarski92@gmail.com>
2020-12-02 12:07:42 +0100

    first commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,32 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        //testowanie kalkulatora
+        Calculator adding = new Calculator(5,5);
+        if (adding.Add() == 10){
+            System.out.println("Dodawanie działa poprawnie");
+        } else {
+            System.out.println("Dodawanie nie działa poprawnie");
+        }
+
+        if (adding.substract() == 0){
+            System.out.println("Odejmowanie działa poprawnie");
+        } else {
+            System.out.println("Odejmowanie nie działa poprawnie");
+        }
     }
 }
